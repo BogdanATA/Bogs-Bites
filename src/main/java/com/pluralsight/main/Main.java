dcf1953b20327e9061fe09cfbff4a10c13c959d6
BogdanATA <batameev@gmail.com>
2026-05-26 11:43:24 -0400

    Created Home Screen and Order Screen Added helper methods for user input inside `UserInterface`

--- src/main/java/com/pluralsight/main/Main.java
+++ src/main/java/com/pluralsight/main/Main.java
@@ -1,7 +1,10 @@
 package com.pluralsight.main;
 
+import com.pluralsight.ui.UserInterface;
+
 public class Main {
     public static void main(String[] args) {
-
+        UserInterface ui = new UserInterface();
+        ui.displayHomeScreen();
     }
 }
--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -1,4 +1,92 @@
 package com.pluralsight.ui;
 
+import com.pluralsight.models.Order;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class UserInterface {
+    private Scanner scanner;
+    private List<Order> order;
+
+    public UserInterface(){
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void displayHomeScreen() {
+        boolean running = true;
+        while (running) {
+            System.out.println("=== Welcome to Bog's Bites ===");
+            System.out.println("1. New Order");
+            System.out.println("0. Exit");
+
+            int choice = readInt("Enter your choice: ");
+
+            switch (choice) {
+                case 1 -> displayOrderScreen();
+                case 0 -> {
+                    System.out.println("GoodBye!");
+                    running = false;
+                }
+                default -> System.out.println("Invalid choice.");
+            }
+        }
+    }
+
+    public void displayOrderScreen() {
+        boolean running = true;
+        while (running) {
+            System.out.println("\n=== Order ===");
+            System.out.println("1. Add Sandwich");
+            System.out.println("2. Add Drink");
+            System.out.println("3. Add Chips");
+            System.out.println("4. Checkout");
+            System.out.println("0. Cancel Order");
+
+            int choice = readInt("Enter your choice");
+
+            switch (choice) {
+                case 1 -> System.out.println("Add Sandwich");
+                case 2 -> System.out.println("Add Drink");
+                case 3 -> System.out.println("Add Chips");
+                case 4 -> System.out.println("Checkout");
+                case 0 -> {
+                    System.out.println("Order Cancelled");
+                    running = false;
+                }
+                default -> System.out.println("Invalid Input");
+            }
+        }
+    }
+
+
+
+
+    /*---------------------------------------------------------------
+     *                       Helper Methods
+     * --------------------------------------------------------------*/
+    private String readString(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine().trim();
+    }
+
+    private int readInt(String prompt) {
+        while (true) {
+            try {
+                return Integer.parseInt(readString(prompt));
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, please enter a whole number.");
+            }
+        }
+    }
+
+    private double readDouble(String prompt) {
+        while (true) {
+            try {
+                return Double.parseDouble(readString(prompt));
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, please enter a number");
+            }
+        }
+    }
 }
